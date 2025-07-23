f6697cc56d74e6ed2462679f06837afc9f7a10de
Navinraj2405 <anavinraj1100@gmail.com>
2025-07-23 18:37:34 +0530

    updated files

--- index.js
+++ index.js
@@ -30,28 +30,4 @@ closenav.addEventListener("click",function(){
         });
 
 
-        //   var timedelay=document.querySelector(".transition")
-        //   function delay(){
-        // transition.style.display="block"
-
-        //   }
-        //   setTimeout(delay,3000)
-
-        // slideimage
-        var slideIndex = 1;
-        showDivs(slideIndex);
-
-        function plusDivs(n) {
-            showDivs(slideIndex += n);
-        }
-
-        function showDivs(n) {
-            var i;
-            var x = document.getElementsByClassName("mySlides");
-            if (n > x.length) { slideIndex = 1 }
-            if (n < 1) { slideIndex = x.length }
-            for (i = 0; i < x.length; i++) {
-                x[i].style.display = "none";
-            }
-            x[slideIndex - 1].style.display = "block";
-        }
+        
